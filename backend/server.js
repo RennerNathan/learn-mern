a932c8213adb22d56d59119956c2edfba06d464a
Nathan Renner <nathanrenner68@gmail.com>
2022-04-13 10:44:42 -0500

    create goal routes and controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+// if frontend hits /api/goals, it'll look into goal routes
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+
+app.listen(port, () => console.log(`server started on ${port}`))
