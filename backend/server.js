ab540dccd716f47e737d01d4f2b08d9c183c2e99
Nathan Renner <nathanrenner68@gmail.com>
2022-04-13 11:01:46 -0500

    error Middleware and add express async handler

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 const app = express()
 
+// MIDDLEWARE
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+
 // if frontend hits /api/goals, it'll look into goal routes
 app.use('/api/goals', require('./routes/goalRoutes'))
 
+// override express default error handler
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`server started on ${port}`))
